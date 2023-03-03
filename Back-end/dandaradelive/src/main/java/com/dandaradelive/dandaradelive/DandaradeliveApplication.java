13b9ef5fa1f4533519f51a00cfdfa09bea0a8d99
bluiz <brunoluizperez@gmail.com>
2023-03-03 01:10:24 -0300

    projeto criado

---
+++ Back-end/dandaradelive/src/main/java/com/dandaradelive/dandaradelive/DandaradeliveApplication.java
@@ -0,0 +1,13 @@
+package com.dandaradelive.dandaradelive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DandaradeliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DandaradeliveApplication.class, args);
+	}
+
+}
