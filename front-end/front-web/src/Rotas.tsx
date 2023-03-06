eba5d2a59e3a0fdc3560f81cc9ef96fc78045620
bluiz <brunoluizperez@gmail.com>
2023-03-06 11:27:09 -0300

    listagem de produtos

---
+++ front-end/front-web/src/Rotas.tsx
@@ -0,0 +1,21 @@
+
+import {
+  BrowserRouter,  Routes,  Route } from "react-router-dom";
+import Home from "./Home/index";
+import Navbar from "./Navbar";
+import Orders from "./Orders";
+
+function Rota() {
+  return (
+    <BrowserRouter> 
+     <Navbar />    
+        <Routes>
+          <Route path='/' element={<Home />} />             
+          <Route path='/Orders' element={<Orders />} />          
+        </Routes>
+   
+    </BrowserRouter>
+  )
+}
+
+export default Rota;
